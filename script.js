8b51995902570c1eefe5382abc43bc60e2004da2
Jared Eklin <jaredeklin@gmail.com>
2017-12-16 08:54:25 -0700

    Add jquery and save click functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+$('.save-button').on('click', function (event) {
+  event.preventDefault();
+  console.log('click')
+  var ideaTitle = $('.idea-title-input').val();
+  var ideaBody = $('.idea-input').val();
+  console.log( ideaTitle, ideaBody);
+  $('.bottom-section').prepend(`
+    <article class="idea">
+      <h3 class="idea-title-output">${ideaTitle}</h3>
+      <button class="remove-button"></button>
+      <p class="idea-body">${ideaBody}</p>
+      <button class="up-vote vote-area"></button>
+      <button class="down-vote vote-area"></button>
+      <p class="quality-id vote-area">quality: swill</p>
+    </article>`);
+})
+
+
